4dfac66e44c2eb446160739640c37b28f369ea33
Manu <mcabral@frba.utn.edu.ar>
2022-11-02 02:44:25 -0300

    feat: add post user from controller

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { get } = require('../controllers/user')
+const { get, post } = require('../controllers/user')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.post('/', post)
 
 module.exports = router
