1fa04edac3d64d539db6102fba5ee43b4f26c489
Manu <mcabral@frba.utn.edu.ar>
2022-11-02 15:43:28 -0300

    feat: add get one user router

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { get, createUser } = require('../controllers/user')
+const { get, createUser, userData } = require('../controllers/user')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.get('/:id', userData)
 router.post('/', createUser)
 
 module.exports = router
