6b94836e20395437c07a3f7be25ee23762f4f853
Manu <mcabral@frba.utn.edu.ar>
2022-11-02 03:00:14 -0300

    feat: encrypt user password
    also add some verifications and refactors

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const createHttpError = require('http-errors')
 const { User } = require('../database/models')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
+const { genSaltSync, hashSync } = require('bcrypt')
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
@@ -21,15 +22,23 @@ module.exports = {
             next(httpError)
         }
     }),
-    post: catchAsync(async (req, res, next) => {
+    createUser: catchAsync(async (req, res, next) => {
         try {
             const { firstname, lastname, email, password } = req.body
             if (!firstname || !lastname || !email || !password)
                 throw new Error('Not all fields have been entered.')
             const existUser = await User.findOne({ where: { email } })
             if (existUser)
-                throw new Error('An account with this email already exists.')
-            const response = await User.create(req.body)
+                throw new Error('Email already exists.')
+            const hashPassword = hashSync(password, genSaltSync(10))
+            if(!hashPassword) throw new Error('Could not hash the password.')
+            const response = await User.create({
+                firstName: firstname,
+                lastName: lastname,
+                password: hashPassword,
+                email,
+            })
+            if(!response) throw new Error('Could not create the user.')
             endpointResponse({
                 res,
                 message: 'User created successfully',
