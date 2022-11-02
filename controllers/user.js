1a6cd1ca0d6c20dd95b18c3e30e3465fef79ae85
Manu <mcabral@frba.utn.edu.ar>
2022-11-02 02:43:50 -0300

    feat: add create user

--- controllers/user.js
+++ controllers/user.js
@@ -21,4 +21,26 @@ module.exports = {
             next(httpError)
         }
     }),
+    post: catchAsync(async (req, res, next) => {
+        try {
+            const { firstname, lastname, email, password } = req.body
+            if (!firstname || !lastname || !email || !password)
+                throw new Error('Not all fields have been entered.')
+            const existUser = await User.findOne({ where: { email } })
+            if (existUser)
+                throw new Error('An account with this email already exists.')
+            const response = await User.create(req.body)
+            endpointResponse({
+                res,
+                message: 'User created successfully',
+                body: response,
+            })
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error creating user] - [user - POST]: ${error.message}`,
+            )
+            next(httpError)
+        }
+    }),
 }
