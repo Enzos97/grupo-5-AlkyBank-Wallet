b673cecfdc0c501c259c23bd0ed85d3c65a93ac0
delgadoagustin <delgadoagustin147@gmail.com>
2022-11-02 20:35:12 -0300

    Add post route to create category

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { get, getById } = require('../controllers/category')
+const { get, getById, createCategory } = require('../controllers/category')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
 router.get('/:id', getById)
+router.post('/',createCategory)
 
 module.exports = router
