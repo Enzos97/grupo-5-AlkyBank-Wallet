7c09abad795166d2ccd57691df566b67a39dd697
delgadoagustin <delgadoagustin147@gmail.com>
2022-11-02 22:09:33 -0300

    Added editCategory Controller and Route

--- controllers/category.js
+++ controllers/category.js
@@ -5,6 +5,7 @@ const { catchAsync } = require('../helpers/catchAsync')
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
+    
   get: catchAsync(async (req, res, next) => {
     try {
         const response = await Category.findAll();
@@ -20,6 +21,30 @@ module.exports = {
             `[Error retrieving index] - [index - GET]: ${error.message}`,
         )
         next(httpError)
+        }
+    }),
+    editCategory: catchAsync(async (req, res, next) => {
+        try {
+            const {id} = req.params
+            const {name, description} = req.body
+            const response = await Category.update({
+                name,
+                description
+            },{
+                where: {id: id}
+            });
+            // @ts-ignore
+            endpointResponse({
+                res,
+                message: "Category updated successfully",
+                body: response,
+            })
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error Updating Category] - [Category - PUT]: ${error.message}`,
+            )
+            next(httpError)
         }
     }),
 }
--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { get } = require('../controllers/category')
+const { get, editCategory } = require('../controllers/category')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.put('/:id', editCategory)
 
 module.exports = router
