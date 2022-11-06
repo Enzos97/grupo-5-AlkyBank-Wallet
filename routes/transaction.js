519d6bd363cad43bdbe530beea1d4ee142cfe9a9
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2022-11-06 20:52:06 -0300

    adding endpoint transactions/id

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { createTransaction } = require('../controllers/transaction')
+const { createTransaction,transactionData } = require('../controllers/transaction')
 const { schemaValidator } = require('../middlewares/validateHelper')
-const { newTransaction } = require('../schemas/transactionNew')
+const { newTransaction} = require('../schemas/transactionNew')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.post('/', schemaValidator(newTransaction), createTransaction)
+router.get('/:id',transactionData)
 
 
 module.exports = router
