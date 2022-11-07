a47e8295b0e9a477f188eb0b700064374f9255d2
manuel14mds <manuel14mds@gmail.com>
2022-11-07 16:38:01 -0500

    decode encode veryfy jwt

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,18 +4,18 @@ const config = require('../config/config')
 
 // entity:user{}
 const encode = (entity)=>{
-    let token = jwt.sign(entity, config.jwSecret, {
-        expiresIn:"1h"
+    let token = jwt.sign(entity, config.development.jwtSecret, {
+        expiresIn:config.development.jwtExpire
     })
     return token
 }
 const decode = (token)=>{
-    let entity = jwt.verify(token, config.jwSecret)
+    let entity = jwt.verify(token, config.development.jwtSecret)
     return entity
 }
 // token:token, entity:user{}
 const verify = (token, entity)=>{
-    let entityUncoded = jwt.verify(token, config.jwSecret)
+    let entityUncoded = jwt.verify(token, config.development.jwtSecret)
     if(!entityUncoded)return null
 
     if(entityUncoded.id === entity.id){
