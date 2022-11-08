96cd13ed7301cc15c9cc6f41cbdce9443420246a
manucabral <mcabral@frba.utn.edu.ar>
2022-11-08 00:02:47 -0300

    test(users): add POST /users endpoint

--- test/users.test.js
+++ test/users.test.js
@@ -9,6 +9,14 @@ const app = require('../app');
 // test suite
 describe('TEST /users', () => {
 
+    let userId;
+    const userToCreate = {
+        firstName: 'Test',
+        lastName: 'Here!',
+        email: 'test@gmail.com',
+        password: '%S!1Q%4V5kjW',
+    }
+
     describe('GET /users', async () => {
         it('SUCCESS: should return all users (list)', async () => {
             const response = await request(app).get('/users');
@@ -21,6 +29,17 @@ describe('TEST /users', () => {
             const response = await request(app).get('/users/100');
             expect(response.status).to.equal(404);
             expect(response.body).to.be.an('object');
+        });
+    });
+
+    describe('POST /users', async () => {
+        it('SUCCESS: should create a new user', async () => {
+            const response = await request(app).post('/users').send(userToCreate);
+            expect(response.status).to.equal(200);
+            expect(response.body).to.be.an('object');
+            expect(response.body.status).to.equal(true);
+            expect(response.body.code).to.equal(200);
+            userId = response.body.body.id;
         });
     });
 
