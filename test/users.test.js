000131e328614a246a11cf6b9544d55e53e19889
manucabral <mcabral@frba.utn.edu.ar>
2022-11-07 23:37:06 -0300

    test: add users base

---
+++ test/users.test.js
@@ -0,0 +1,12 @@
+// required modules
+const request = require('supertest');
+const expect = require('chai').expect;
+// const { User } = require('../database/models');
+
+// setting up the server
+const app = require('../app');
+
+// test suite
+describe('TEST /users', () => {
+    
+});
