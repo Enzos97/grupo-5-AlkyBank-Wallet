2decfb81086d344a174e4a9b8e71e5f06ecde9e0
manucabral <mcabral@frba.utn.edu.ar>
2022-11-07 23:41:22 -0300

    test(users): add GET /users endpoint

--- test/users.test.js
+++ test/users.test.js
@@ -8,5 +8,15 @@ const app = require('../app');
 
 // test suite
 describe('TEST /users', () => {
+
+    describe('GET /users', async () => {
+        it('SUCCESS: should return all users (list)', async () => {
+            const response = await request(app).get('/users');
+            expect(response.status).to.equal(200);
+            expect(response.body).to.be.an('object');
+            expect(response.body.status).to.equal(true);
+            expect(response.body.code).to.equal(200);
+        });
+    });
     
 });
