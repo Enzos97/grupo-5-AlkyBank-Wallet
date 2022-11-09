78d3027ac12187fe17971e63aef570843425ba29
manuel14mds <manuel14mds@gmail.com>
2022-11-08 21:21:00 -0500

    auth user fixed, changed request body by request params

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,13 +1,16 @@
 const {ErroObject} = require("../helpers/error")
 const {verify} = require("../helpers/jwt")
 const { User } = require('../database/models')
+const { INTEGER } = require("sequelize")
 
 module.exports={
     auth : async (req,res,next)=>{
+        let userId = parseInt(req.params.id)
+        if(!userId)throw new ErroObject('bad request id', 400)
         let token = req.headers.authorization.split(" ")[1]
         if(!token) throw new ErroObject('access denied', 401)
         let result = verify(token, req.body)
-        let user = await User.findOne({ where: { email:req.body.email } })
+        let user = await User.findOne({ where: { id:userId } })
         if(result){
             req.user = user
             next()
