b1b28659d7819a63b31e2679e2725e30add4ebcc
manuel14mds <manuel14mds@gmail.com>
2022-11-10 14:33:55 -0500

    auth user update, token validation

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,21 +1,20 @@
 const {ErroObject} = require("../helpers/error")
-const {verify} = require("../helpers/jwt")
+const {verify, decode} = require("../helpers/jwt")
 const { User } = require('../database/models')
 const { INTEGER } = require("sequelize")
 
 module.exports={
     auth : async (req,res,next)=>{
-        let userId = parseInt(req.params.id)
-        if(!userId)throw new ErroObject('bad request id', 400)
         let token = req.headers.authorization.split(" ")[1]
-        if(!token) throw new ErroObject('access denied', 401)
-        let result = verify(token, req.body)
-        let user = await User.findOne({ where: { id:userId } })
-        if(result){
+        if(!token)throw new ErroObject('No token', 400)
+        let userToken = decode(token)
+        let user = await User.findOne({ where: { id:userToken.id } })
+        if(user){
             req.user = user
             next()
         }else{
-            throw new ErroObject('access denied', 401)
+            throw new ErroObject('Invalid token', 403)
         }
     }
 }
+
