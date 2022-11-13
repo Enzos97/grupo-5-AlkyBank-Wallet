fd3e670f51f6a1ea03c0e554dbc660d7f8dfc62d
manuel14mds <manuel14mds@gmail.com>
2022-11-13 09:57:20 -0500

    errorObject fixed

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,4 +1,4 @@
-const {ErroObject} = require("../helpers/error")
+const {ErrorObject} = require("../helpers/error")
 const {verify, decode} = require("../helpers/jwt")
 const { User } = require('../database/models')
 const { INTEGER } = require("sequelize")
@@ -6,14 +6,14 @@ const { INTEGER } = require("sequelize")
 module.exports={
     auth : async (req,res,next)=>{
         let token = req.headers.authorization.split(" ")[1]
-        if(!token)throw new ErroObject('No token', 400)
+        if(!token)throw new ErrorObject('No token', 400)
         let userToken = decode(token)
         let user = await User.findOne({ where: { id:userToken.user.id } })
         if(user){
             req.user = user
             next()
         }else{
-            throw new ErroObject('Invalid token', 403)
+            throw new ErrorObject('Invalid token', 403)
         }
     }
 }
