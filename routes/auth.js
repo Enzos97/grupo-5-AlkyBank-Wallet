84541ecb78739fc2c14f0fb6d230abddb3bb6e13
oajuarez <ojuarez@fi.uba.ar>
2022-11-13 12:24:54 -0300

    Users documentacion

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,75 @@ const { loginSchema } = require("../schemas/users")
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     auth:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: The auto-generated id of the auth
+ *         password:
+ *           type: string
+ *           description: auth's name
+ *       required:
+ *         - name
+ *         - email
+ *       example:
+ *        email: "x@xavierinstitutes.edu"
+ *        password: "CIg9m5ye89Ovq:BV"
+ *       
+ *     authNotFound:
+ *       type: object
+ *       properties:
+ *         msg:
+ *          type: string
+ *          description: A message for the not found auth
+ *       example:
+ *          msg: auth was not found
+ *     authResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *          token: string
+ *   
+ */
+
+
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Auth
+  *   description: Auth endpoints
+  */
+
+
+ /**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *    summary: logs an user account
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/auth'
+ *    responses:
+ *      200:
+ *        description: autentication token
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/AuthResponse'
+ *      500:
+ *        description: Some server error
+ *
+ */
 router.post('/login', schemaValidator(loginSchema), login)
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,200 @@ const { auth } = require('../middlewares/authUser')
 const router = express.Router()
 
 // example of a route with index controller get function
+
+
+// example of a route with index controller get function
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the User
+ *         firstName:
+ *           type: string
+ *           description: User's name
+ *         lastName:
+ *           type: string
+ *           description: User's lastname
+ *         email:
+ *           type: string
+ *           description: User's email
+ *         password:
+ *           type: string
+ *           description: User's password
+ * 
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - password
+ *       example:
+ *         firstName: Bruce
+ *         lastName: Wayne 
+ *         email: bruce@wayne.inc
+ *         password: sdfasd0'SSFDSAXFSDAF
+ *       
+ *     UserNotFound:
+ *       type: object
+ *       properties:
+ *         msg:
+ *          type: string
+ *          description: A message for the not found User
+ *       example:
+ *          msg: User was not found
+ *     UserResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *          type: boolean
+ *         code:
+ *          type: number
+ *   
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT          
+ * 
+ *   parameters:
+ *    UserId:
+ *      in: path
+ *      name: id
+ *      required: true
+ *      schema:
+ *       type: string
+ *      description: User id         
+ *
+ */
+
+
+ /**
+ * @swagger
+ * /users?page=1:
+ *   get:
+ *     summary: Returns the list of all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the Users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 router.get('/', get)
 
+
+  /**
+ * @swagger
+ * /users/{id}:
+ *  get:
+ *    security:
+ *     - bearerAuth: []
+ *    summary: get a User by Id
+ *    tags: [Users]
+ *    parameters:
+ *      - $ref: '#/components/parameters/UserId'
+ *    responses:
+ *      200:
+ *        description: The found User
+ *        content:
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/User'
+ *      404:
+ *        description: the User was not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UserNotFound'
+ */
 router.get('/:id', auth, userData)
+
+/**
+ * @swagger
+ * /users:
+ *  post:
+ *    summary: create a new User
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: the User created successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UserResponse'
+ *      500:
+ *        description: Some server error
+ *
+ */
 router.post('/', uploadImage,schemaValidator(register),createUser)
-router.delete('/:id', auth, deletedUser)
+
+
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     security:
+ *      - bearerAuth: []
+ *     summary: Edit an user's info
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Edit an user's info
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
 router.put('/:id', auth,schemaValidator(edit),editUser)
 
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  delete:
+ *    security:
+ *     - bearerAuth: []
+ *    summary: deletes a User by Id
+ *    tags: [Users]
+ *    parameters:
+ *      - $ref: '#/components/parameters/UserId'
+ *    responses:
+ *      200:
+ *        description: The deleted user
+ *        content:
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/User'
+ *      404:
+ *        description: the User was not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UserNotFound'
+ */
+ router.delete('/:id', auth, deletedUser)
+
 module.exports = router
+
+
+
+
